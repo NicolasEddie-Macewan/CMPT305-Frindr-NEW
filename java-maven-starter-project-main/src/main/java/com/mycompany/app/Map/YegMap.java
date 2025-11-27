e1fb4703aa2ae5a2632e7cedaa9b4b317eff7d28
dalkamal <daliak_3@hotmail.com>
2025-11-27 16:58:30 -0700

    adding map folder

---
+++ java-maven-starter-project-main/src/main/java/com/mycompany/app/Map/YegMap.java
@@ -0,0 +1,10 @@
+package com.mycompany.app.ui;
+
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.VBox;
+
+import java.util.List;
+
+public class YegMap
+}
